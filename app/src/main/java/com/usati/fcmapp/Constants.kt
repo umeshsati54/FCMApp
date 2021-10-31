0f938792b91da8122f062c7c533c9bfe950ea579
Umesh Sati <umeshsati54@gmail.com>
2021-10-31 11:16:43 +0530

    Update Constants.kt

--- app/src/main/java/com/usati/fcmapp/Constants.kt
+++ app/src/main/java/com/usati/fcmapp/Constants.kt
@@ -4,8 +4,8 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY =
-            "AAAAb14S4Mk:APA91bHcctDcuVHda9dwCKfyKiEjFnha6w1BKzVett6VFkircxiadseeEpoTg3gvEufEG2hy0xLbLq5eJmhOWTb4CTBRJw6SnAqeVLtxwtnM1l_sAfMfD0ClpwYmS1XTz1cb2zFEVHr0"
+        const val SERVER_KEY = "Server Key"
+
         const val CONTENT_TYPE = "application/json"
     }
 }
